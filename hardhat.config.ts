99e6c81b5f0a95ed14a25bc3d84dbdf476984ff5
gg2001 <gautham.gg@gmail.com>
2021-04-05 13:52:50 +1000

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+import "./tasks/clean";
+
+dotenv.config();
+const ALCHEMY_MAINNET: string = "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY;
+const COINMARKETCAP: string | undefined = process.env.COINMARKETCAP;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: ALCHEMY_MAINNET,
+        blockNumber: 12088078,
+      },
+      chainId: 1337,
+    },
+  },
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  mocha: {
+    timeout: 600000,
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 100,
+    coinmarketcap: COINMARKETCAP,
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+};
+
+export default config;
